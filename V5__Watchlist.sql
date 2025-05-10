c508f6b2c11351b91a88a2200b513769e97e769e
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-05-10 18:39:05 +0530

    feat: watchlist database

---
+++ V5__Watchlist.sql
@@ -0,0 +1,12 @@
+CREATE TYPE stock_exchange AS ENUM ('NSE', 'BSE', 'MCX', 'NCDEX');
+
+CREATE TABLE user_stock_watchlist (
+    user_id INT NOT NULL,
+    isin VARCHAR(12) NOT NULL,
+    exchange stock_exchange NOT NULL,
+    position_index INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT PK_User_Stock_Watchlist PRIMARY KEY (user_id, isin, exchange),
+    CONSTRAINT FK_User_To_Watchlist FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
