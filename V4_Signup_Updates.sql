b2ce4c37998923bb1f413480ff9777ced51d41d2
Dhruv Arora <dhruv862arora@gmail.com>
2025-05-10 16:27:41 +0530

    refactor : made necessary changes in Db

--- V4_Signup_Updates.sql
+++ V4_Signup_Updates.sql
@@ -13,3 +13,37 @@ ALTER TABLE signup_checkpoints ADD COLUMN occupation occupation;
 
 -- politically exposed person
 ALTER TABLE signup_checkpoints ADD COLUMN IF NOT EXISTS is_politically_exposed BOOLEAN;
+
+
+CREATE TYPE nominee_relation AS ENUM (
+    'Father',
+    'Mother',
+    'Son', 
+    'Daughter',
+    'Sister',
+    'Brother',
+    'Spouse',
+    'Other'
+);
+
+ALTER TABLE nominees
+DROP CONSTRAINT IF EXISTS FK_Nominees_Pan,
+DROP CONSTRAINT IF EXISTS FK_Nominees_Aadhaar;
+
+ALTER TABLE nominees_to_checkpoint
+DROP CONSTRAINT IF EXISTS FK_Nominees_To_Checkpoint_Nominees_Id;
+
+ALTER TABLE nominees
+DROP COLUMN IF EXISTS pan_id,
+DROP COLUMN IF EXISTS aadhaar_id,
+DROP COLUMN IF EXISTS relationship,
+ADD COLUMN IF NOT EXISTS id_type VARCHAR(10) NOT NULL CHECK (id_type IN ('PAN', 'AADHAR')),
+ADD COLUMN IF NOT EXISTS gov_id VARCHAR(16) NOT NULL,
+ADD COLUMN IF NOT EXISTS relation nominee_relation NOT NULL,
+ADD COLUMN IF NOT EXISTS created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+ALTER COLUMN share TYPE INT USING (share::INT),
+ADD CONSTRAINT CHK_Share CHECK (share > 0 AND share <= 100);
+
+ALTER TABLE nominees_to_checkpoint
+ADD CONSTRAINT FK_Nominees_To_Checkpoint_Nominees_Id FOREIGN KEY (nominees_id) REFERENCES nominees (id) ON DELETE CASCADE;
