9b6ffcc4c54b7b81b21a38f57ad650a08579094c
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-05-10 19:30:54 +0530

    feat: unique position index per user

--- V5__Watchlist.sql
+++ V5__Watchlist.sql
@@ -8,5 +8,6 @@ CREATE TABLE user_stock_watchlist (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT PK_User_Stock_Watchlist PRIMARY KEY (user_id, isin, exchange),
-    CONSTRAINT FK_User_To_Watchlist FOREIGN KEY (user_id) REFERENCES "user" (id)
+    CONSTRAINT FK_User_To_Watchlist FOREIGN KEY (user_id) REFERENCES "user" (id),
+    CONSTRAINT UQ_User_Position_Index UNIQUE (user_id, position_index)
 );
