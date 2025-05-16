44aea443d21a70b14ff68ec45df70640f6659adc
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-05-17 00:39:02 +0530

    fix: nominees table

--- V4__Signup_Updates.sql
+++ V4__Signup_Updates.sql
@@ -1,29 +1,24 @@
-CREATE TYPE occupation AS ENUM ('student', 'govt.servant', 'retired', 'private sector', 'agriculturalist', 'self-employed', 'housewife', 'other');
+CREATE TYPE occupation AS ENUM ('student', 'govt servant', 'retired', 'private sector', 'agriculturalist', 'self employed', 'housewife', 'other');
 CREATE TYPE account_type AS ENUM ('savings', 'current');
 CREATE TYPE nominee_relation AS ENUM ('Father', 'Mother', 'Son', 'Daughter', 'Sister', 'Brother', 'Spouse', 'Other');
 
-ALTER TABLE bank_account 
-  ADD COLUMN account_type account_type DEFAULT 'savings';
+ALTER TABLE bank_account
+    ADD COLUMN account_type account_type DEFAULT 'savings';
 
-ALTER TABLE bank_account 
-  ALTER COLUMN account_type DROP DEFAULT;
+ALTER TABLE bank_account
+    ALTER COLUMN account_type DROP DEFAULT;
 
-ALTER TABLE signup_checkpoints 
-  ALTER COLUMN occupation TYPE occupation USING 'self-employed'::occupation;
-
-ALTER TABLE nominees 
-  ADD COLUMN id_type VARCHAR(10) NOT NULL DEFAULT 'PAN', 
-  ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL, 
-  ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL;
-ALTER TABLE nominees
-  ALTER COLUMN relationship TYPE nominee_relation USING 'Other'::nominee_relation;
-
-ALTER TABLE nominees 
-  ADD CONSTRAINT CHK_Share CHECK (share > 0 AND share <= 100);
+ALTER TABLE signup_checkpoints
+    ALTER COLUMN occupation TYPE occupation USING 'self employed'::occupation;
 
 ALTER TABLE nominees
-  ADD CONSTRAINT CHK_ID_Type_Consistency 
-  CHECK (
-    (id_type = 'PAN' AND pan_id IS NOT NULL AND aadhaar_id IS NULL) OR
-    (id_type = 'AADHAAR' AND aadhaar_id IS NOT NULL AND pan_id IS NULL)
-  );
+    ADD COLUMN govt_id    VARCHAR(12)             NOT NULL DEFAULT '',
+    ADD COLUMN created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    ADD COLUMN updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    ALTER COLUMN relationship TYPE nominee_relation USING 'Other'::nominee_relation,
+    DROP CONSTRAINT FK_Nominees_Pan,
+    DROP CONSTRAINT FK_Nominees_Aadhaar,
+    DROP COLUMN aadhaar_id,
+    DROP COLUMN pan_id,
+    ALTER COLUMN govt_id DROP DEFAULT,
+    ADD CONSTRAINT CHK_Share CHECK (share > 0.0 AND share <= 100.0);
