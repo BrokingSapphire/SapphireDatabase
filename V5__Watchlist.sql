c28c8e8024c245ca3dc9bb6e88a8631c396d6f62
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-05-22 11:00:38 +0530

    feat: watchlist position index
    Signed-off-by: AbhigyaKrishna <abhigyakrishna21@gmail.com>

--- V5__Watchlist.sql
+++ V5__Watchlist.sql
@@ -16,32 +16,45 @@ CREATE TABLE watchlist_category
     CONSTRAINT UQ_Watchlist_Category UNIQUE (category)
 );
 
-CREATE TABLE user_stock_watchlist
+CREATE TABLE user_watchlist
 (
     id             SERIAL,
     user_id        INT       NOT NULL,
     watchlist_id   INT,
-    category_id    INT,
     position_index INT       NOT NULL,
     created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT PK_User_Stock_Watchlist PRIMARY KEY (id),
-    CONSTRAINT FK_User_To_Watchlist FOREIGN KEY (user_id) REFERENCES "user" (id),
-    CONSTRAINT FK_Watchlist_To_User FOREIGN KEY (watchlist_id) REFERENCES watchlist (id),
-    CONSTRAINT FK_Category_To_Watchlist FOREIGN KEY (category_id) REFERENCES watchlist_category (id),
-    CONSTRAINT UQ_User_Stock_Watchlist UNIQUE (user_id, watchlist_id, category_id),
-    CONSTRAINT UQ_User_Category_Position_Index UNIQUE (user_id, watchlist_id, position_index)
+    CONSTRAINT PK_User_Watchlist PRIMARY KEY (id),
+    CONSTRAINT FK_User_Watchlist_User FOREIGN KEY (user_id) REFERENCES "user" (id),
+    CONSTRAINT FK_User_Watchlist_Watchlist FOREIGN KEY (watchlist_id) REFERENCES watchlist (id),
+    CONSTRAINT UQ_User_Watchlist UNIQUE (user_id, watchlist_id),
+    CONSTRAINT UQ_User_Watchlist_Position UNIQUE (user_id, position_index)
+);
+
+CREATE TABLE watchlist_category_map
+(
+    id                SERIAL,
+    user_watchlist_id INT       NOT NULL,
+    category_id       INT,
+    position_index    INT       NOT NULL,
+    created_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT PK_Watchlist_Category_Map PRIMARY KEY (id),
+    CONSTRAINT FK_Watchlist_Category_Map_User_Watchlist FOREIGN KEY (user_watchlist_id) REFERENCES user_watchlist (id),
+    CONSTRAINT FK_Watchlist_Category_Map_Category FOREIGN KEY (category_id) REFERENCES watchlist_category (id),
+    CONSTRAINT UQ_Watchlist_Category UNIQUE (user_watchlist_id, category_id),
+    CONSTRAINT UQ_Watchlist_Category_Position UNIQUE (user_watchlist_id, position_index)
 );
 
 CREATE TABLE user_watchlist_entry
 (
-    user_watchlist_id INT            NOT NULL,
-    isin              VARCHAR(12)    NOT NULL,
-    exchange          stock_exchange NOT NULL,
-    position_index    INT            NOT NULL,
-    created_at        TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at        TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT PK_User_Watchlist_Entry PRIMARY KEY (user_watchlist_id, isin, exchange),
-    CONSTRAINT FK_Watchlist_Entry FOREIGN KEY (user_watchlist_id) REFERENCES user_stock_watchlist (id),
-    CONSTRAINT UQ_Watchlist_Entry_Position_Index UNIQUE (user_watchlist_id, position_index)
+    category_map_id INT            NOT NULL,
+    isin            VARCHAR(12)    NOT NULL,
+    exchange        stock_exchange NOT NULL,
+    position_index  INT            NOT NULL,
+    created_at      TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT PK_User_Watchlist_Entry PRIMARY KEY (category_map_id, isin, exchange),
+    CONSTRAINT FK_Watchlist_Entry_Category_Map FOREIGN KEY (category_map_id) REFERENCES watchlist_category_map (id),
+    CONSTRAINT UQ_Watchlist_Entry_Position_Index UNIQUE (category_map_id, position_index)
 );
