e2831db88951e5e46970a53cabce44fc9a711c7a
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-05-22 10:16:53 +0530

    feat: category index in watchlist
    Signed-off-by: AbhigyaKrishna <abhigyakrishna21@gmail.com>

--- V5__Watchlist.sql
+++ V5__Watchlist.sql
@@ -18,17 +18,19 @@ CREATE TABLE watchlist_category
 
 CREATE TABLE user_stock_watchlist
 (
-    id           SERIAL,
-    user_id      INT       NOT NULL,
-    watchlist_id INT,
-    category_id  INT,
-    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id             SERIAL,
+    user_id        INT       NOT NULL,
+    watchlist_id   INT,
+    category_id    INT,
+    position_index INT       NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT PK_User_Stock_Watchlist PRIMARY KEY (id),
     CONSTRAINT FK_User_To_Watchlist FOREIGN KEY (user_id) REFERENCES "user" (id),
     CONSTRAINT FK_Watchlist_To_User FOREIGN KEY (watchlist_id) REFERENCES watchlist (id),
     CONSTRAINT FK_Category_To_Watchlist FOREIGN KEY (category_id) REFERENCES watchlist_category (id),
-    CONSTRAINT UQ_User_Stock_Watchlist UNIQUE (user_id, watchlist_id, category_id)
+    CONSTRAINT UQ_User_Stock_Watchlist UNIQUE (user_id, watchlist_id, category_id),
+    CONSTRAINT UQ_User_Category_Position_Index UNIQUE (user_id, watchlist_id, position_index)
 );
 
 CREATE TABLE user_watchlist_entry
@@ -41,5 +43,5 @@ CREATE TABLE user_watchlist_entry
     updated_at        TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT PK_User_Watchlist_Entry PRIMARY KEY (user_watchlist_id, isin, exchange),
     CONSTRAINT FK_Watchlist_Entry FOREIGN KEY (user_watchlist_id) REFERENCES user_stock_watchlist (id),
-    CONSTRAINT UQ_User_Watchlist_Position_Index UNIQUE (user_watchlist_id, position_index)
+    CONSTRAINT UQ_Watchlist_Entry_Position_Index UNIQUE (user_watchlist_id, position_index)
 );
